76c27e7cd11f141a65b55016a0da6fc28f01a858
Alena <nikishova-alena@mail.ru>
2025-02-26 12:55:06 +0300

    add models

--- server/index.js
+++ server/index.js
@@ -1,19 +1,25 @@
-import dotenv from 'dotenv';
-import express from "express";
-
-
-dotenv.config();
+require('dotenv').config();
+const express = require('express');
+const sequelize = require('./db');
+const models = require('./models/models.js');
 
 const PORT = process.env.PORT || 5000
 
 const app = express();
 app.use(express.json());
 
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, () => console.log('server started: ', PORT));
+    } catch (e) {
+        console.log(e)
+    }
+}
 
+start()
 
 app.get('/', (req, res) => {
     res.send('ti loh')
 })
-
-
-app.listen(PORT, () => console.log('server started: ', PORT));
